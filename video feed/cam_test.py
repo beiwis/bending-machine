e96b6d3471c354e51f9ba6f8a7107af56acee1f2
beiwis <bearoco@gmail.com>
2023-11-18 13:26:30 +0100

    Created camera test

---
+++ video feed/cam_test.py
@@ -0,0 +1,23 @@
+import cv2
+
+def show_video_feed():
+    # Create a VideoCapture object to capture video from the camera
+    cap = cv2.VideoCapture(0)  # 0 represents the default camera index
+
+    while True:
+        # Read the current frame from the camera
+        ret, frame = cap.read()
+
+        # Display the frame in a window named "Video Feed"
+        cv2.imshow("Video Feed", frame)
+
+        # Break the loop if the 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the VideoCapture object and close all windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+# Call the function to start showing the video feed
+show_video_feed()
